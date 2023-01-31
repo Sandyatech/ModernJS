9b1b72c57027eef3a604039181849b1b6776a2f7
sandyatech <sandhya.mskgmail.com>
2023-01-31 20:08:20 +0000

    updated add function with reduce built in function

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,7 +1,5 @@
-// Instructions can be found in rest_parameters.md
 
-export function add(...args) {
-    let sum = 0;
-    for (let arg of args) sum += arg;
-    return sum;
+
+export function add(...numbers) {
+    return numbers.reduce((total, number) => total += number, 0);
 }
